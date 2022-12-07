f7e4a71649ee834a9c60f97f5ea6662d4d8c9e7b
mariepuhakka <mariesusanpuhakka@gmail.com>
2022-12-07 13:51:50 +0200

    step 5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useState} from 'react'
+
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick = {props.handleClick}>
+        {props.text}
+      </button>
+    </>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <>
+      <p>
+        {props.text} {props.number}
+      </p>
+    </>
+  )
+}
+
+
+const Statistics = (props) => {
+  const sum = props.good + props.neutral + props.bad
+  const average = (props.good * 1 + props.bad * (-1)) / sum 
+  const positive = (props.good / sum) * 100 + ' %' 
+  if (sum >= 1) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <StatisticLine text = 'good' number = {props.good}/>
+        <StatisticLine text = 'neutral' number = {props.neutral}/>
+        <StatisticLine text = 'bad' number = {props.bad}/>
+        <StatisticLine text = 'Sum' number = {sum}/>
+        <StatisticLine text = 'Average' number = {average}/>
+        <StatisticLine text = 'Positive' number = {positive}/>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback given!</p>
+      </>
+    )
+  }
+  
+}
 
 function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Give feedback</h1>
+      <Button text = 'good' handleClick = {handleGood}/>
+      <Button text = 'neutral' handleClick = {handleNeutral}/>
+      <Button text = 'bad' handleClick = {handleBad}/>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
+    </>
   );
 }
 
